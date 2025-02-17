ac8955ef8de9fce1dd44d93d1fb17f2975c69389
dfuzethehostage <dfuzethehostage@gmail.com>
2025-02-17 15:10:15 +0100

    drawing and deleting

--- src/index.js
+++ src/index.js
@@ -2,42 +2,77 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const container = document.querySelector(".canvas-container");
 
+let scale = 1,
+  panning = false,
+  drawing = false,
+  deleting = false,
+  start = { x: 0, y: 0 },
+  lineWidth = 2,
+  gridHeight = 300,
+  gridWidth = 400,
+  minScale = 0.7,
+  maxScale = 15,
+  hideLineScale = 3,
+  colorLines = "#666",
+  colorSquares = rgb(0, 0, 0);
+
+let fields = [];
+for (let i = 0; i < gridHeight; i++) {
+  let row = [];
+  for (let j = 0; j < gridWidth; j++) {
+    row.push(0);
+  }
+  fields.push(row);
+}
+
 // Setze Canvas-Größe
-let lineWidth = 2;
-let gridHeight = 40;
-let gridWidth = 60;
 canvas.height = container.clientHeight;
 canvas.width = container.clientHeight * (gridWidth / gridHeight);
-container.clientWidth = canvas.width;
-console.log(container);
-// Grid-Parameter
-let cellSize = canvas.height / gridHeight; // Größe der Zellen in Pixeln
-let offsetX = 0; // Verschiebung in X-Richtung
-let offsetY = 0; // Verschiebung in Y-Richtung
+
+canvas.width = canvas.width - (canvas.width % gridWidth);
+canvas.height = canvas.height - (canvas.height % gridHeight);
+
+canvas.style.width = canvas.width + "px";
+canvas.style.height = canvas.height + "px";
+
+canvas.height *= 8;
+canvas.width *= 8;
+
+let cellSize = canvas.height / gridHeight;
+let pointX = canvas.offsetLeft,
+  pointY = canvas.offsetTop;
+
+setTransform();
 
 // Zeichne das Grid
 function drawGrid() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.lineWidth = lineWidth;
-  ctx.strokeStyle = "#000";
+  ctx.strokeStyle = "#666";
+  ctx.fillStyle = "black";
 
-  // Berechne den Startpunkt für das Grid
-  const startX = offsetX % cellSize;
-  const startY = offsetY % cellSize;
+  for (let i = 0; i < gridHeight; i++) {
+    for (let j = 0; j < gridWidth; j++) {
+      if (fields[i][j] == 1) {
+        ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
+      }
+    }
+  }
+
+  if (scale <= hideLineScale) return;
 
   // **Vertikale Linien**
-  for (let i = 0; i <= gridWidth; i++) {
-    let x = startX + i * cellSize;
+  for (let i = 1; i < gridWidth; i++) {
+    let x = i * cellSize;
     ctx.beginPath();
     ctx.moveTo(x, 0);
     ctx.lineTo(x, canvas.height);
     ctx.stroke();
   }
-
   // **Horizontale Linien**
-  for (let i = 0; i <= gridHeight; i++) {
-    let y = startY + i * cellSize;
+  for (let i = 1; i < gridHeight; i++) {
+    let y = i * cellSize;
     ctx.beginPath();
     ctx.moveTo(0, y);
     ctx.lineTo(canvas.width, y);
@@ -47,43 +82,79 @@ function drawGrid() {
 
 drawGrid();
 
-var scale = 1,
-  panning = false,
-  pointX = 0,
-  pointY = 0,
-  start = { x: 0, y: 0 };
-
 function setTransform() {
-  console.log(pointX, pointY);
-  canvas.style.transform = `translate(${pointX}px, ${pointY}px) scale(${scale})`;
+  canvas.style.transform = `translate(${pointX - canvas.offsetLeft}px, ${
+    pointY - canvas.offsetTop
+  }px) scale(${scale})`;
+  drawGrid();
+}
+
+function getFieldCoords(e) {
+  e.preventDefault();
+  mouseX = e.clientX - pointX;
+  mouseY = e.clientY - pointY;
+  let x = Math.floor(mouseX / ((canvas.offsetHeight / gridHeight) * scale));
+  let y = Math.floor(mouseY / ((canvas.offsetHeight / gridHeight) * scale));
+  return { y: y, x: x };
 }
 
 canvas.onmousedown = function (e) {
-  e.preventDefault();
-  start = { x: e.clientX - pointX, y: e.clientY - pointY };
-  panning = true;
+  if (e.button == 2) {
+    e.preventDefault();
+    start = { x: e.clientX - pointX, y: e.clientY - pointY };
+    panning = true;
+  } else if (e.button == 0) {
+    let coords = getFieldCoords(e),
+      y = coords.y,
+      x = coords.x;
+    if (fields[y][x] == 0) {
+      drawing = true;
+      fields[y][x] = 1;
+    } else if (fields[y][x] == 1) {
+      deleting = true;
+      fields[y][x] = 0;
+    }
+    drawGrid(fields);
+  }
 };
 
 window.onmouseup = function (e) {
-  panning = false;
+  if (e.button == 2) {
+    panning = false;
+  } else if (e.button == 0) {
+    drawing = false;
+    deleting = false;
+  }
 };
 
-container.onmousemove = function (e) {
-  e.preventDefault();
-  if (!panning) {
-    return;
+canvas.onmousemove = function (e) {
+  if (e.button == 2) {
+    e.preventDefault();
+    if (!panning) {
+      return;
+    }
+    pointX = e.clientX - start.x;
+    pointY = e.clientY - start.y;
+    setTransform();
+  } else if (e.button == 0) {
+    let coords = getFieldCoords(e),
+      x = coords.x,
+      y = coords.y;
+    if (drawing) fields[y][x] = 1;
+    else if (deleting) fields[y][x] = 0;
+    drawGrid();
   }
-  pointX = e.clientX - start.x;
-  pointY = e.clientY - start.y;
-  setTransform();
 };
 
 window.onwheel = function (e) {
   e.preventDefault();
-  var xs = (e.clientX - pointX) / scale,
+  let xs = (e.clientX - pointX) / scale,
     ys = (e.clientY - pointY) / scale;
   delta = e.wheelDelta ? e.wheelDelta : -e.deltaY;
-  scale *= delta < 0 ? (scale >= 0.7 ? 1 / 1.1 : 1) : 1.1;
+
+  if (delta < 0 && scale >= minScale) scale *= 1 / 1.1;
+  else if (delta > 0 && scale <= maxScale) scale *= 1.1;
+
   pointX = e.clientX - xs * scale;
   pointY = e.clientY - ys * scale;
 
